c2f5bb5d5b0614237a2760fb1114d445cbe7b0fd
Holt Crews <holtcrews47@gmail.com>
2023-07-07 14:56:20 -0400

    Fix formatting :

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -6,7 +6,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb',builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 
