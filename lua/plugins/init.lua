ff42893568cfe24f2363ba65a0173f7618fad1aa
hcrews47 <holtcrews47@gmail.com>
2023-07-09 18:08:48 -0400

    Remove old file, update lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,6 +47,14 @@ return {
                 component_separators = '|',
                 section_separators = '',
             },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'buffers' },
+                lualine_x = { 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            }
         },
     },
     {
