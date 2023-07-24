c6014841e6906aeedb6e1f9438c4bf452ff6ca7f
Holt Crews <holtcrews47@gmail.com>
2023-07-24 10:14:37 -0400

    Remove format on save

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -33,24 +33,27 @@ local lSsources = {
   }),
 }
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- used for format on save
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
   sources = lSsources,
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = bufnr,
-            filter = function(client)
-              return client.name == "null-ls"
-            end,
-          })
-        end,
-      })
-    end
+    -- this chunk formats on save
+    -- if client.supports_method("textDocument/formatting") then
+    --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --
+    -- vim.api.nvim_create_autocmd("BufWritePre", {
+    --   group = augroup,
+    --   buffer = bufnr,
+    --   callback = function()
+    --     vim.lsp.buf.format({
+    --       bufnr = bufnr,
+    --       filter = function(client)
+    --         return client.name == "null-ls"
+    --       end,
+    --     })
+    --   end,
+    -- })
+    -- end
   end,
 })
