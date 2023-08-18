5622775e93bed18f5d5c9146d29eb13fdb1686d0
Holt Crews <holtcrews47@gmail.com>
2023-08-18 17:18:29 -0400

    Update laziness of vim fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   "mg979/vim-visual-multi",
   "tpope/vim-sleuth",
   "nvim-treesitter/nvim-treesitter-context",
-  "tpope/vim-fugitive",
+  { "tpope/vim-fugitive", cmd = { "G", "Git" } },
   { "tpope/vim-surround", event = "BufReadPost" },
   { "folke/which-key.nvim", keys = { "<leader>", '"', "`", "c", "v", "g" }, opts = {} },
   { "folke/trouble.nvim", opts = { icons = false }, cmd = "Trouble" },
