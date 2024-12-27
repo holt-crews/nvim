c3e58f53aff391798a2fd9e877a1b20bc72dd443
Holt Crews <holtcrews47@gmail.com>
2024-12-26 21:55:52 -0600

    fix: update packages

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,7 +28,6 @@ return {
     init = function()
       vim.opt.termguicolors = true
       vim.opt.background = "dark"
-      vim.g.gruvbox_material_disable_italic_comment = 1
       vim.g.gruvbox_material_enable_bold = 1
       vim.g.gruvbox_material_better_performance = true
       vim.g.gruvbox_material_cursor = "green"
